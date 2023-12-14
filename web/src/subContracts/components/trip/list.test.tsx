ae7eb4586302d73cfbbd32d1b113589196c7b651
Barath <barath@Baraths-Mac-mini.local>
2023-12-14 13:20:05 +0530

    written test for web/trip/list.test.ts

--- web/src/subContracts/components/trip/list.test.tsx
+++ web/src/subContracts/components/trip/list.test.tsx
@@ -2,34 +2,51 @@ import { render, screen, waitFor } from '@testing-library/react'
 import TripList from './list'
 import { BrowserRouter } from 'react-router-dom'
 import { vi } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import TransporterPay from './transporterPay'
 
 const mockAllTrip = vi.fn()
+const mockTripByVehicleNumber = vi.fn()
 
 vi.mock('../../services/trip', () => ({
-    getAllTrip: () => mockAllTrip()
+    getAllTrip: () => mockAllTrip(),
+    getTripByVehicleNumber: () => mockTripByVehicleNumber()
 }))
 
-describe('Trip Test', () => {
-    test('should fetch data from Db', async () => {
-        const mockTripData = [
-            {
-                startDate: 1696934739,
-                factory: {
-                    name: 'salem'
-                },
-                deliveryPoint: {
-                    name: 'erode'
-                },
-                truck: {
-                    vehicleNumber: 'TN93D5512',
-                    transporter: {
-                        name: 'Barath'
-                    }
-                },
-                transporterBalance: 1000
+const mockTripData = [
+    {
+        startDate: 1696934739,
+        factory: {
+            name: 'salem'
+        },
+        deliveryPoint: {
+            name: 'erode'
+        },
+        truck: {
+            vehicleNumber: 'TN93D5512',
+            transporter: {
+                name: 'Barath'
             }
-        ]
+        },
+        transporterBalance: 1000
+    }
+]
+
+const byVechicleNumber = {
+    id: 1,
+    totalTransporterAmount: 50000,
+    truck: {
+        transporter: { name: 'Something' }
+    }
+}
+
+describe('Trip Test', () => {
+    beforeEach(() => {
         mockAllTrip.mockResolvedValue(mockTripData)
+        mockAllTrip.mockResolvedValue(mockTripData)
+        mockTripByVehicleNumber.mockResolvedValue(byVechicleNumber)
+    })
+    test('should fetch data from Db', async () => {
         render(
             <BrowserRouter>
                 <TripList />
@@ -41,5 +58,32 @@ describe('Trip Test', () => {
             expect(screen.getByText('salem')).toBeInTheDocument()
         })
         expect(mockAllTrip).toHaveBeenCalledTimes(1)
+    })
+    test('checking the component called NewTrip', async () => {
+        render(
+            <BrowserRouter>
+                <TransporterPay />
+            </BrowserRouter>
+        )
+        const options2 = screen.getByRole('combobox', { name: 'Truck Number' })
+        userEvent.click(options2)
+        await waitFor(() => screen.getByRole('listbox'))
+        const opt2 = screen.getByRole('option', { name: 'TN93D5512' })
+        userEvent.click(opt2)
+        expect(await screen.findByDisplayValue('TN93D5512')).toBeInTheDocument()
+        expect(mockAllTrip).toHaveBeenCalledTimes(3)
+
+        expect(screen.getByText('Paid First Payment')).toBeInTheDocument()
+
+        expect(screen.getByRole('spinbutton', { name: 'Seventy Percentage' }))
+        expect(screen.getByDisplayValue('35000')).toBeVisible()
+
+        expect(screen.getByRole('textbox', { name: 'Transporter' }))
+        expect(screen.getByDisplayValue('Something')).toBeVisible()
+
+        expect(screen.getByRole('spinbutton', { name: 'Remaining' }))
+        expect(screen.getByDisplayValue('15000')).toBeVisible()
+
+        screen.debug()
     })
 })
