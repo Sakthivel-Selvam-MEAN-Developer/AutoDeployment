6cb3b51f293eec661c4f12e6806dafab90980fe9
Barath <barath@Baraths-Mac-mini.local>
2024-01-22 11:18:51 +0530

    cleared errors and daily Update

--- backend/src/subContracts/controller/acknowledgement.test.ts
+++ backend/src/subContracts/controller/acknowledgement.test.ts
@@ -21,15 +21,46 @@ const mockOverAllTrip = [
     {
         id: 1,
         acknowledgementStatus: false,
+        stockPointToUnloadingPointTripId: null,
+        loadingPointToUnloadingPointTripId: 1,
         loadingPointToStockPointTrip: null,
         loadingPointToUnloadingPointTrip: {
             id: 1,
             truck: {
                 vehicleNumber: 'TN93D5512'
+            },
+            loadingPoint: {
+                id: 1,
+                name: 'Chennai-south'
+            },
+            unloadingPoint: {
+                id: 1,
+                name: 'Salem'
             }
         }
     }
 ]
+const mockOverAllTripByIdData = {
+    id: 1,
+    acknowledgementStatus: false,
+    stockPointToUnloadingPointTripId: null,
+    loadingPointToUnloadingPointTripId: 1,
+    loadingPointToStockPointTrip: null,
+    loadingPointToUnloadingPointTrip: {
+        id: 1,
+        truck: {
+            vehicleNumber: 'TN93D5512'
+        },
+        loadingPoint: {
+            id: 1,
+            name: 'Chennai-south'
+        },
+        unloadingPoint: {
+            id: 1,
+            name: 'Salem'
+        }
+    }
+}
 const mockCloseTripData = {
     id: 1,
     tripStatus: false
@@ -38,39 +69,21 @@ const mockCloseTripData = {
 describe('Acknowledgement Controller', () => {
     test('should able to get all vehicle number from overAllTrip', async () => {
         mockAllTripByAcknowledgementStatus.mockResolvedValue(mockOverAllTrip)
-        await supertest(app).get('/acknowledgement').expect(200)
+        await supertest(app).get('/api/acknowledgement').expect(200)
         expect(mockAllTripByAcknowledgementStatus).toBeCalledTimes(1)
     })
     test('should able to get trip Details from overAllTrip by Id ', async () => {
-        mockOverAllTripById.mockResolvedValue({
-            id: 1,
-            acknowledgementStatus: false,
-            loadingPointToStockPointTrip: null,
-            loadingPointToUnloadingPointTrip: {
-                id: 1,
-                truck: {
-                    vehicleNumber: 'TN93D5512'
-                }
-            }
-        })
-        await supertest(app)
-            .get('/acknowledgement/:id')
-            .expect({
-                id: 1,
-                acknowledgementStatus: false,
-                loadingPointToStockPointTrip: null,
-                loadingPointToUnloadingPointTrip: {
-                    id: 1,
-                    truck: {
-                        vehicleNumber: 'TN93D5512'
-                    }
-                }
-            })
+        mockOverAllTripById.mockResolvedValue(mockOverAllTripByIdData)
+        await supertest(app).get('/api//acknowledgement/:id').expect(mockOverAllTripByIdData)
         expect(mockOverAllTripById).toBeCalledTimes(1)
     })
-    test.skip('should able to close trip by Id ', async () => {
-        mockOverAllTripById.mockResolvedValue(mockCloseTripData)
-        await supertest(app).put('/acknowledgement/trip').expect(mockCloseTripData)
+    test('should able to close trip by Id ', async () => {
+        mockOverAllTripById.mockResolvedValue(mockOverAllTripByIdData)
+        mockcloseStockTrip.mockResolvedValue(mockCloseTripData)
+        mockcloseTrip.mockResolvedValue(mockCloseTripData)
+        await supertest(app).put('/api/acknowledgement/trip').expect(200)
+        expect(mockOverAllTripById).toBeCalledTimes(2)
         expect(mockcloseTrip).toBeCalledTimes(1)
+        expect(mockcloseStockTrip).toBeCalledTimes(0)
     })
 })
