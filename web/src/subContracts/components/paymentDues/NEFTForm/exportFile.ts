b83fef105d7c99579a8fafb8dc0a63ad04713022
Sakthivel Selvam <141139829+Sakthivel-Selvam-MEAN-Developer@users.noreply.github.com>
2024-03-07 18:15:56 +0530

    changes in NEFT from generation

--- web/src/subContracts/components/paymentDues/NEFTForm/exportFile.ts
+++ web/src/subContracts/components/paymentDues/NEFTForm/exportFile.ts
@@ -25,7 +25,7 @@ export const exportFile = async (NEFTData: dataProps[]) => {
         if (data.type === 'fuel pay')
             NEFTDataBody += `${data.bankDetails[0].ifsc},${data.bankDetails[0].accountTypeNumber},${data.bankDetails[0].accountNumber},${data.bankDetails[0].bunkName},${data.bankDetails[0].location},Magnum-Fuel,${data.payableAmount}\n`
         else {
-            if (data.type === 'inital pay') type = 'Magnum-Advance'
+            if (data.type === 'initial pay') type = 'Magnum-Advance'
             else if (data.type === 'final pay') type = 'Magnum-Final'
             else if (data.type === 'gst pay') type = 'Magnum-GST'
             NEFTDataBody += `${data.bankDetails[0].ifsc},${data.bankDetails[0].accountTypeNumber},${data.bankDetails[0].accountNumber},${data.bankDetails[0].name},${data.bankDetails[0].address},${type},${data.payableAmount}\n`
