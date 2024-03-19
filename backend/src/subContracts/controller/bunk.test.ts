d7556b2a63f8c31145a3f58f9d4060308b2f5761
Sakthivel Selvame <sakthiselvamcs@outlook.com>
2024-03-19 14:21:36 +0530

    added parameter validation for test

--- backend/src/subContracts/controller/bunk.test.ts
+++ backend/src/subContracts/controller/bunk.test.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest'
 import { Prisma } from '@prisma/client'
+import { Request, Response } from 'express'
 import { app } from '../../app.ts'
+import { createBunk } from './bunk.ts'
 
 const mockCreateBunk = vi.fn()
 const mockBunkDetails = vi.fn()
@@ -11,14 +13,36 @@ vi.mock('../models/bunk', () => ({
 }))
 
 const mockBunk = {
-    bunkName: 'Bharath Petroleum'
+    body: {
+        bunkName: 'Bharath Petroleum',
+        location: 'London',
+        accountHolder: 'Barath',
+        accountNumber: '123ABC',
+        ifsc: '1234XYZA',
+        accountTypeNumber: 121212
+    }
 }
-
+const mockReq = {
+    body: {
+        bunkName: 'Bharath Petroleum',
+        location: 'London',
+        accountHolder: 'Barath',
+        accountNumber: '123ABC',
+        ifsc: '1234XYZA',
+        accountTypeNumber: 121212
+    }
+} as Request
+const mockRes = {
+    sendStatus: vi.fn(),
+    status: vi.fn()
+} as unknown as Response
 describe('Bunk Controller', () => {
     test('should able to create Bunk', async () => {
         mockCreateBunk.mockResolvedValue(mockBunk)
+        createBunk(mockReq, mockRes)
+        expect(mockCreateBunk).toHaveBeenCalledWith(mockReq.body)
         await supertest(app).post('/api/bunk').expect(200)
-        expect(mockCreateBunk).toBeCalledTimes(1)
+        expect(mockCreateBunk).toBeCalledTimes(2)
     })
     test('should able to access', async () => {
         mockBunkDetails.mockResolvedValue({ bunkName: 'Bharath Petroleum' })
