29c034ab6ee88f7a8236691f5a077a0feb53989a
Sakthivel Selvame <sakthiselvamcs@outlook.com>
2024-04-01 16:49:40 +0530

    User Based Authentication Temporarily Commented

--- backend/src/subContracts/routes/authorise.ts
+++ backend/src/subContracts/routes/authorise.ts
@@ -1,19 +1,30 @@
-import keycloak from '../../keycloak-config.ts'
+// import keycloak from '../../keycloak-config.ts'
+// import { Role } from '../roles.ts'
+// import logger from '../../logger.ts'
+
+// const hasValidRole = (roles: Role[], token: any) => {
+//     roles.push('SuperAdmin')
+//     const tokenRoles = token.content.realm_access.roles
+//     logger.info('Token roles:', tokenRoles)
+//     console.log(roles.find((role) => tokenRoles.includes(role)) !== undefined)
+//     return roles.find((role) => tokenRoles.includes(role)) !== undefined
+// }
+
+// export const authorise = (roles: Role[] = ['Employee']) => {
+//     const validateTokenRoles = (token: any) => {
+//         if (token.isExpired()) return false
+//         return hasValidRole(roles, token)
+//     }
+//     return keycloak.protect(validateTokenRoles)
+// }
+
+import { NextFunction, Request, Response } from 'express'
 import { Role } from '../roles.ts'
 import logger from '../../logger.ts'
 
-const hasValidRole = (roles: Role[], token: any) => {
-    roles.push('SuperAdmin')
-    const tokenRoles = token.content.realm_access.roles
-    logger.info('Token roles:', tokenRoles)
-    console.log(roles.find((role) => tokenRoles.includes(role)) !== undefined)
-    return roles.find((role) => tokenRoles.includes(role)) !== undefined
-}
-
-export const authorise = (roles: Role[] = ['Employee']) => {
-    const validateTokenRoles = (token: any) => {
-        if (token.isExpired()) return false
-        return hasValidRole(roles, token)
+export const authorise = (roles: Role[] = []) => {
+    logger.info('role', roles)
+    return (_req: Request, _res: Response, next: NextFunction) => {
+        next()
     }
-    return keycloak.protect(validateTokenRoles)
 }
