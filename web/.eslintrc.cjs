8d2425057ec7fd268d7f1235e3092cb5d078da25
deepak <anandhadeepak@gmail.com>
2024-04-13 19:47:56 +0530

    updating jsx specific lint rules

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -5,10 +5,11 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
+        'plugin:react/all'
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh','react'],
     overrides: [
         {
             files: ['*.test.tsx', '*.test.ts'],
@@ -17,17 +18,41 @@ module.exports = {
                 'max-lines-per-function': ['error', 500],
                 "@typescript-eslint/no-explicit-any": "off"
             }
+        },
+        {
+            files: ['*.tsx'],
+            parserOptions: {
+                "ecmaFeatures": {
+                    "jsx": true
+                }
+            },
+            rules: {
+                'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+                'max-lines-per-function': ['warn', {max: 50, skipBlankLines: true}],
+                'react/function-component-definition': ['error', {
+                    "namedComponents": "arrow-function" ,
+                    "unnamedComponents":  "arrow-function"
+                }],
+                "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+                "react/jsx-no-bind": ['error', {
+                    "allowArrowFunctions": true,
+                }],
+                "react/jsx-indent": ['off'],
+                "react/react-in-jsx-scope": "off",
+                "react/prop-types" : "off",
+                "react/jsx-max-depth": ["warn", { "max": 2 }],
+                "react/jsx-no-literals":"off",
+            }
         }
     ],
     rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         semi: [2, 'never'],
         indent: 'off',
         'operator-linebreak': 0,
         'function-paren-newline': 0,
         'max-lines': ['warn', 50],
         'max-depth': ['error', 3],
-        'max-lines-per-function': ['warn', 10],
+        'max-lines-per-function': ['warn', {max: 10, skipBlankLines: true}],
         'import/prefer-default-export': 0,
         '@typescript-eslint/no-explicit-any': 'warn',
         "complexity": ["warn", { "max": 2 }]
