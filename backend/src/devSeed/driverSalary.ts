4bd7df53a84b55182c2fb9f24348fec0583a0f67
Sakthivel Selvam <141139829+Sakthivel-Selvam-MEAN-Developer@users.noreply.github.com>
2024-04-24 18:41:30 +0530

    rollbacked ifsc to ifcs

--- backend/src/devSeed/driverSalary.ts
+++ backend/src/devSeed/driverSalary.ts
@@ -1,7 +1,7 @@
 import prisma from '../../prisma/index.ts'
 
 async function seedSubContract() {
-    await prisma.$executeRaw`INSERT INTO "driverSalary"."driver" ("name","fatherName","dateofBirth","aadharNumber","panNumber","address","mobileNumber","driverLicense","licenseExpriryDate","bankName","accountNumber","accountBranch","ifscCode", "createdAt", "updatedAt")
+    await prisma.$executeRaw`INSERT INTO "driverSalary"."driver" ("name","fatherName","dateofBirth","aadharNumber","panNumber","address","mobileNumber","driverLicense","licenseExpriryDate","bankName","accountNumber","accountBranch","ifcsCode", "createdAt", "updatedAt")
     VALUES ('sakthi','father',3456781,'dummyaadhar','dummypan','summa','09876543','ghj5678',4567,'newBank','2345678345678','branch','ifsc1234', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);`
 }
 export default seedSubContract
