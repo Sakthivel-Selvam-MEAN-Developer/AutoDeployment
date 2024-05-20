03141bc9ed6819fa233fdd8354f0418a397c1498
Barath <barathsam555.p@gmail.com>
2024-05-20 17:08:13 +0530

    logic change in transporter invoice

--- backend/src/subContracts/controller/transporterInvoice.ts
+++ backend/src/subContracts/controller/transporterInvoice.ts
@@ -7,15 +7,14 @@ export const listTripByTransporterInvoice = (_req: Request, res: Response) => {
         .then((data) => res.status(200).json(data))
         .catch(() => res.status(500))
 }
-const checkPreFinalPay = (data: object, finalPay: undefined | object, res: Response) => {
-    if (finalPay !== undefined) return res.status(200).json(data)
-}
 export const updateTransporterInvoiceinTrip = (req: Request, res: Response) => {
     updateTransporterInvoice(req.body.invoice, req.body.id)
         .then(async (data) => {
             if (data.acknowledgementStatus === false) return res.status(200).json(data)
-            const finalPay = data.paymentDues.find((due) => due.type === 'final pay')
-            checkPreFinalPay(data, finalPay, res)
+            const finalPay = data.paymentDues.filter((due) => due.type === 'final pay')
+            if (finalPay.length > 0) {
+                return res.status(200).json(data)
+            }
             await finalDueCreation(data, res)
         })
         .catch(() => res.status(500))
