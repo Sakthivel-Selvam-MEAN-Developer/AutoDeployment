80c6d86f66fcff2b2d99ca57286fc3f51c1d9894
Ramya <ramya@Ramyas-Mac-mini.local>
2024-06-28 18:06:25 +0530

    fixed migration error

--- backend/prisma/migrations/20240628053205_added_overalltrip_id_and_type_combination_to_be_unique_when_type_is_not_equal_to_fuel_pay/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_pkey" TO "tollPayment_pkey";
-
--- RenameForeignKey
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_overallTripId_fkey" TO "tollPayment_overallTripId_fkey";
-
--- RenameForeignKey
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_tollPlazaLocationId_fkey" TO "tollPayment_tollPlazaLocationId_fkey";
