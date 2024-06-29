95bfe25ba40be15b6a401661a3ea183a6091d6fb
Sakthivel Selvam <sakthivelselvam@Sakthivels-Mac-mini.local>
2024-06-29 17:41:01 +0530

    Added tollPayment and tollLocations and companyInvoice

--- backend/prisma/migrations/20240626050630_name_change_toll_plaza_to_toll_payment/migration.sql
+++
@@ -1,7 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the `tollPlaza` table. If the table is not empty, all the data it contains will be lost.
-
-*/
-ALTER TABLE "subContract"."tollPlaza" RENAME TO "tollPayment";
--- backend/prisma/migrations/20240629120753_added_toll_payment_and_toll_locations_and_added_company_invoice/migration.sql
+++ backend/prisma/migrations/20240629120753_added_toll_payment_and_toll_locations_and_added_company_invoice/migration.sql
@@ -7,9 +7,6 @@ ALTER TABLE "subContract"."loadingPointToUnloadingPointTrip" ADD COLUMN     "com
 -- AlterTable
 ALTER TABLE "subContract"."stockPointToUnloadingPointTrip" ADD COLUMN     "companyInvoiceId" INTEGER;
 
--- AlterTable
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_pkey" TO "tollPayment_pkey";
-
 -- CreateTable
 CREATE TABLE "subContract"."companyInvoice" (
     "id" SERIAL NOT NULL,
@@ -24,11 +21,31 @@ CREATE TABLE "subContract"."companyInvoice" (
     CONSTRAINT "companyInvoice_pkey" PRIMARY KEY ("id")
 );
 
--- RenameForeignKey
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_overallTripId_fkey" TO "tollPayment_overallTripId_fkey";
+-- CreateTable
+CREATE TABLE "subContract"."tollPayment" (
+    "id" SERIAL NOT NULL,
+    "overallTripId" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "billNo" TEXT,
+    "billDate" INTEGER,
+    "tollPlazaLocationId" INTEGER,
+    "billedStatus" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
 
--- RenameForeignKey
-ALTER TABLE "subContract"."tollPayment" RENAME CONSTRAINT "tollPlaza_tollPlazaLocationId_fkey" TO "tollPayment_tollPlazaLocationId_fkey";
+    CONSTRAINT "tollPayment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subContract"."tollPlazaLocation" (
+    "id" SERIAL NOT NULL,
+    "location" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "tollPlazaLocation_pkey" PRIMARY KEY ("id")
+);
 
 -- AddForeignKey
 ALTER TABLE "subContract"."companyInvoice" ADD CONSTRAINT "companyInvoice_cementCompanyId_fkey" FOREIGN KEY ("cementCompanyId") REFERENCES "subContract"."cementCompany"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -41,3 +58,9 @@ ALTER TABLE "subContract"."stockPointToUnloadingPointTrip" ADD CONSTRAINT "stock
 
 -- AddForeignKey
 ALTER TABLE "subContract"."loadingPointToUnloadingPointTrip" ADD CONSTRAINT "loadingPointToUnloadingPointTrip_companyInvoiceId_fkey" FOREIGN KEY ("companyInvoiceId") REFERENCES "subContract"."companyInvoice"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subContract"."tollPayment" ADD CONSTRAINT "tollPayment_overallTripId_fkey" FOREIGN KEY ("overallTripId") REFERENCES "subContract"."overallTrip"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subContract"."tollPayment" ADD CONSTRAINT "tollPayment_tollPlazaLocationId_fkey" FOREIGN KEY ("tollPlazaLocationId") REFERENCES "subContract"."tollPlazaLocation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
