bf5c6c6641b74360631d9318b9cbf95f53c5c782
Ramya <ramya@Ramyas-Mac-mini.local>
2024-07-09 19:04:46 +0530

    created edit button for transpoter page

--- web/src/subContracts/components/transporter/transporterReport/transporterReportShow.tsx
+++ web/src/subContracts/components/transporter/transporterReport/transporterReportShow.tsx
@@ -1,13 +1,18 @@
+import React, { useState } from 'react'
 import Table from '@mui/material/Table'
-import React from 'react'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
 import { CircularLoader } from '../../cementCompany/companyReport/companyReportShow'
-
 interface Props {
     allTransporter: Row[]
     loading: boolean
@@ -29,28 +34,30 @@ interface Row {
     accountNumber: string
     ifsc: string
     accountTypeNumber: number
+    createdAt?: string
+    updatedAt?: string
+    id?: string
 }
-
 const cellNames = [
     'Name',
-    'Csm Name',
+    'CSM Name',
     'Email Id',
     'Contact Person Name',
     'Contact Person Number',
     'Address',
-    'HasGst',
+    'GST',
     'GST Number',
     'GST Percentage',
-    'Has Tds',
+    'TDS',
     'Transporter Type',
-    'Tds Percentage',
+    'TDS Percentage',
     'AccountHolder',
     'Account Number',
     'BranchName',
     'IFSC',
-    'AccountType Number'
+    'AccountType',
+    'Actions'
 ]
-
 const tableRow = (
     <TableRow>
         <TableCell>#</TableCell>
@@ -65,53 +72,110 @@ const getTableHead = () => {
     return <TableHead>{tableRow}</TableHead>
 }
 function cell(data: Row) {
-    const cells = Object.entries(data).map(([key, value]) => {
-        if (key == 'createdAt' || key == 'updatedAt' || key == 'id') return
-        return subCell(key, value)
-    })
+    const cells = Object.entries(data)
+        .filter(([key]) => key !== 'createdAt' && key !== 'updatedAt' && key !== 'id')
+        .map(([key, value]) => {
+            return subCell(key, value)
+        })
     return cells
 }
 const style = { '&:last-child td, &:last-child th': { border: 0 } }
-const getTableBody = (allTransporter: Row[]) => {
+const getTableBody = (allTransporter: Row[], handleEdit: (row: Row) => void) => {
     return (
         <TableBody>
-            {allTransporter && allTransporter.map((row, index) => tableBodyRow(index, row))}
+            {allTransporter &&
+                allTransporter.map((row, index) => tableBodyRow(index, row, handleEdit))}
         </TableBody>
     )
 }
-
-const tableContainer = (allTransporter: Row[]) => {
+const tableContainer = (allTransporter: Row[], handleEdit: (row: Row) => void) => {
     return (
         <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
             <Table stickyHeader sx={{ minWidth: 600 }} aria-label="sticky table">
                 {getTableHead()}
-                {getTableBody(allTransporter)}
+                {getTableBody(allTransporter, handleEdit)}
             </Table>
         </TableContainer>
     )
 }
-
 const ListAllTransporter: React.FC<Props> = ({ allTransporter, loading }) => {
+    const [open, setOpen] = useState(false)
+    const [selectedRow, setSelectedRow] = useState<Row | null>(null)
+    const handleEdit = (row: Row) => {
+        setSelectedRow(row)
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+        setSelectedRow(null)
+    }
+    const capitalizeFirstLetter = (string: string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+    const formatKey = (key: string) => {
+        return key
+            .split(/(?=[A-Z])/)
+            .map(capitalizeFirstLetter)
+            .join(' ')
+    }
     return (
         <>
-            {loading ? <CircularLoader /> : tableContainer(allTransporter)}
+            {loading ? <CircularLoader /> : tableContainer(allTransporter, handleEdit)}
             <br />
             <br />
+            <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+                <DialogTitle>Edit Transporter Details</DialogTitle>
+                <DialogContent>
+                    {selectedRow && (
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableBody>
+                                    {Object.entries(selectedRow)
+                                        .filter(
+                                            ([key]) =>
+                                                key !== 'createdAt' &&
+                                                key !== 'updatedAt' &&
+                                                key !== 'id'
+                                        )
+                                        .map(([key, value]) => (
+                                            <TableRow key={key}>
+                                                <TableCell style={{ fontWeight: 'bold' }}>
+                                                    {formatKey(key)}
+                                                </TableCell>
+                                                <TableCell>{value ? value : 'NULL'}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                    <Button onClick={() => console.log('Save Changes')} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
-
 export default ListAllTransporter
-
-function tableBodyRow(index: number, row: Row) {
+function tableBodyRow(index: number, row: Row, handleEdit: (row: Row) => void) {
     return (
         <TableRow key={index} sx={style}>
-            <TableCell> {index + 1} </TableCell>
+            <TableCell>{index + 1}</TableCell>
             {cell(row)}
+            <TableCell align="center">
+                <Button variant="contained" color="primary" onClick={() => handleEdit(row)}>
+                    <ModeEditIcon />
+                </Button>
+            </TableCell>
         </TableRow>
     )
 }
-
 function subCell(key: string, value: string) {
     return (
         <TableCell key={key} align="left">
